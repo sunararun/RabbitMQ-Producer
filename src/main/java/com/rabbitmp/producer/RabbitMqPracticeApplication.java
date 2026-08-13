631bc3066529917ecbfa341da8bfdf699905112f
sunararun <sunararun01@gmail.com>
2026-08-13 09:02:37 +0530

    first commit

---
+++ src/main/java/com/rabbitmp/producer/RabbitMqPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.rabbitmp.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitMqPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rabbitmp/producer/RabbitMqPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabbitmp.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
